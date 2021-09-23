93daff2c107215f50295312d9af75e1068b9613a
Krishnaveni A R <69717422+krishnaveniar@users.noreply.github.com>
2021-09-24 00:06:01 +0530

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,1 +1,11 @@
+import random
+rand_words = ['colorful','comb','neighborly','dangerous','arm','quixotic','superb','omniscient','whine','broken','valuable','fat','examine']
+def get_word():
+    myword= random.choice(rand_words)
+    return myword
+def game():
+    word= get_word()
+    l= len(word)
+    dashes="_" * l
+    print("Let's play Hangman!\n")
 
